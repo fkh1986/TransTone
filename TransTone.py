384dbdf594099eb4a53b59243b76fe393841e6be
Fadi Haddad <fadi.k.haddad@gmail.com>
2017-07-27 00:23:35 +0300

    Generate Sound from Two Sinusoidals

--- TransTone.py
+++ TransTone.py
@@ -1,10 +1,16 @@
 import math
+import pyaudio
+import array
+import numpy as np
+import wave
 
-frequencyR = [697,770, 852, 941]
-frequencyC = [1209, 1336, 1477, 1633]
+frequencyR = (697,770, 852, 941)
+frequencyC = (1209, 1336, 1477, 1633)
 
-samplingFreq = 44000
-tonePeriod = 0.25
+samplingFreq = 44100
+sr = 44100
+tonePeriod = 4
+volume = 0.5
 
 
 digits = {'0' : [4,2],
@@ -26,7 +32,7 @@ digits = {'0' : [4,2],
 
 def GenSineWave(f,fs,t):
     tp = range(0,int(fs*t))
-    sineSig = [math.sin(2*math.pi*f*ts/fs) for ts in tp]
+    sineSig = [np.sin(2*np.pi*f*ts/fs) for ts in tp]
     return sineSig
 
 
@@ -36,10 +42,23 @@ for digit in digits:
     digitFreqs.append([frequencyR[digits[digit][0]-1],frequencyC[digits[digit][1]-1]])
 
 print(digitFreqs)
+print(digitFreqs[0][0])
+print(digitFreqs[0][1])
 
-#test = GenSineWave(1,100,0.5)
-#print(test)
+sin1 = GenSineWave(digitFreqs[0][0],samplingFreq,tonePeriod)
+sin2 = GenSineWave(digitFreqs[0][1],samplingFreq,tonePeriod)
 
+test = [a+b for a,b in zip(sin1, sin2)]
+atest = np.asarray(test).astype(np.float32)
+print(atest)
+print(len(atest))
 
+p = pyaudio.PyAudio()
+stream = p.open(rate=sr, channels = 1, format= pyaudio.paFloat32, output = True)
+
+stream.write(volume*atest)
+
+stream.close()
+p.terminate()
 
 
