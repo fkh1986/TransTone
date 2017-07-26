81a052d6cdbe7ef9e920e5c4dd145824c82ec4e7
Fadi Haddad <fadi.k.haddad@gmail.com>
2017-07-27 02:46:06 +0300

    Add TinyURL encoding to shorten the transmitted codes

--- TransTone.py
+++ TransTone.py
@@ -1,6 +1,7 @@
 import math
 import pyaudio
 import array
+import pyshorteners as pyshort
 import numpy as np
 import wave
 
@@ -9,7 +10,7 @@ frequencyC = (1209, 1336, 1477, 1633)
 
 samplingFreq = 44100
 sr = 44100
-tonePeriod = 0.05
+tonePeriod = 0.1
 volume = 0.5
 
 
@@ -45,25 +46,41 @@ digitFreqs = list()
 for digit in digits:
     digitFreqs.append([frequencyR[digits[digit][0]-1], frequencyC[digits[digit][1]-1]])
 
-print(digitFreqs)
-print(digitFreqs[0][0])
-print(digitFreqs[0][1])
+# print(digitFreqs)
+# print(digitFreqs[0][0])
+# print(digitFreqs[0][1])
+
+URLText = input("Please Enter URL: ")
+shortener = pyshort.Shortener('Tinyurl')
+TinyURL = shortener.short(URLText)
+constURL = "http://tinyurl.com/"
+print(TinyURL.find(constURL))
+lenconstURL = len(constURL)
+StripTinyURL = TinyURL[lenconstURL:]
+print(TinyURL)
+print(StripTinyURL)
+URLHex = StripTinyURL.encode().hex()
+print(URLHex)
 
 
 p = pyaudio.PyAudio()
 stream = p.open(rate=sr, channels=1, format=pyaudio.paFloat32, output=True)
 
-for i in range(15):
+#for i in range(15):
 #    sin1 = GenSineWave(digitFreqs[i][0], samplingFreq, tonePeriod)
 #    sin2 = GenSineWave(digitFreqs[i][1], samplingFreq, tonePeriod)
-    test = GenDTMF(digitFreqs[i][0], digitFreqs[i][1], samplingFreq, tonePeriod, volume)
+#    test = GenDTMF(digitFreqs[i][0], digitFreqs[i][1], samplingFreq, tonePeriod, volume)
 #   test = [a+b for a, b in zip(sin1, sin2)]
 #    atest = np.asarray(test).astype(np.float32)
     #print(atest)
     #print(len(atest))
-    atest = test
+#    atest = test
 
-    stream.write(atest)
+#   stream.write(atest)
+
+for hexdigit in URLHex:
+    test = GenDTMF(digitFreqs[int(hexdigit, 16)][0], digitFreqs[int(hexdigit, 16)][1], samplingFreq, tonePeriod, volume)
+    stream.write(test)
 
 stream.close()
 p.terminate()
