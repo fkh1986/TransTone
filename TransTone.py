20fc90675856f0dab79d98f045b8218af3d37a2f
Fadi Haddad <fadi.k.haddad@gmail.com>
2017-07-27 03:18:23 +0300

    Add comments for the code

--- TransTone.py
+++ TransTone.py
@@ -1,3 +1,9 @@
+# This program is intended to transmit links via audio waves
+# The input for the transmitter station is a valid web link
+# This link is converted to a series of audio tones and played via the speaker output to transmit these tones
+# The receiving station will pickup these audio tones
+# These tones will be decoded back into the original link sent from the transmitting station
+
 import math
 import pyaudio
 import array
@@ -5,14 +11,28 @@ import pyshorteners as pyshort
 import numpy as np
 import wave
 
+# Define Frequency Rows & Columns to generate standard DTMF tones
+
 frequencyR = (697, 770, 852, 941)
 frequencyC = (1209, 1336, 1477, 1633)
 
+# Define couple of constants
+
+# Sampling Frequency to be used to generate the Sine Wave Signals
 samplingFreq = 44100
+
+# Sampling Rate used to play the stream of audio tones
 sr = 44100
+
+# The time spent sending each tone
 tonePeriod = 0.1
+
+# The volume scaling to avoid clipping the audio signal upon addition of sinusoidal waves
 volume = 0.5
 
+# Define the rows and columns for each DTMF digit from 0-F
+# In original DTMF digit E and F do not exist
+# The # and * digits where replaced by E and F in order to be able to transmit data in Hexadecimal format
 
 digits = {'0': [4, 2],
           '1': [1, 1],
@@ -31,9 +51,9 @@ digits = {'0': [4, 2],
           'E': [4, 1],
           'F': [4, 3]}
 
-
+# Generate DTMF from Frequency 1 and Frequency 2 for a period of t seconds, and applying volume scaling
 def GenDTMF(f1, f2, fs, t, volume):
-    sineSig = array.array('f',((volume * math.sin(i / (f1 / 100.)) + volume * math.sin(i / (f2 / 100.)))
+    sineSig = array.array('f',((volume * math.sin(i / (f1 / fs)) + volume * math.sin(i / (f2 / fs)))
                                for i in range(int(fs*t)))).tostring()
     return sineSig
 
